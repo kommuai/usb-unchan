eeff39f41378736b71c94a6b64f040312d748062
Ben Masato <benmasato@kommu.ai>
2021-10-14 22:25:27 +0800

    Add a button with safe reference passing in callback

--- unchan.cpp
+++ unchan.cpp
@@ -1,21 +1,32 @@
 #include <FL/Fl.H>
+#include <FL/Fl_Button.H>
 #include <FL/Fl_Choice.H>
 #include <FL/Fl_Window.H>
 #include <libusb-1.0/libusb.h>
 
+#include <functional>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 #include <string>
 
 
+#define REFWRAP(x)      new std::reference_wrapper<typeof(x)>(x)
+#define UNWRAP(x, t)    ((std::reference_wrapper<t>*) x)->get()
+
 class DeviceList {
 private:
     Fl_Choice *combo;
+    Fl_Button *button;
     libusb_device **list;
 public:
     DeviceList() : combo{NULL}, list{NULL}
     {
         combo = new Fl_Choice(100, 10, 100, 30, "device:");
+        button = new Fl_Button(combo->x() + combo->w() + 10, combo->y(),
+                               60, combo->h(),
+                               "open");
+        button->callback(ButtonOpen, REFWRAP(*this));
     }
 
     ~DeviceList()
@@ -51,6 +62,26 @@ public:
 
             combo->add(o.str().c_str());
         }
+    }
+
+    static void ButtonOpen(Fl_Widget *w, void *p)
+    {
+        DeviceList& d = UNWRAP(p, DeviceList);
+        Fl_Choice *c = d.combo;
+
+        if (c->value() == -1)
+            return;
+
+        libusb_device_descriptor desc;
+        libusb_get_device_descriptor(d.list[c->value()], &desc);
+
+        std::ostringstream o;
+        o << std::hex << std::setfill('0');
+        o << std::setw(4) << desc.idVendor;
+        o << ':';
+        o << std::setw(4) << desc.idProduct;
+
+        std::cout << o.str() << std::endl;
     }
 };
 
