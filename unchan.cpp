06ed19a34723e9725b62957d0083d555a518d48c
Ben Masato <benmasato@kommu.ai>
2021-10-14 20:52:57 +0800

    Refactor device list into class

--- unchan.cpp
+++ unchan.cpp
@@ -1,13 +1,58 @@
 #include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Widget.H>
 #include <FL/Fl_Choice.H>
+#include <FL/Fl_Window.H>
 #include <libusb-1.0/libusb.h>
 
 #include <iomanip>
-#include <string>
 #include <sstream>
+#include <string>
 
+
+class DeviceList {
+private:
+    Fl_Choice *combo;
+    libusb_device **list;
+public:
+    DeviceList() : combo{NULL}, list{NULL}
+    {
+        combo = new Fl_Choice(100, 10, 100, 30, "device:");
+    }
+
+    ~DeviceList()
+    {
+        if (list)
+            libusb_free_device_list(list, 1);
+    }
+
+    void Load()
+    {
+        if (list)
+            libusb_free_device_list(list, 1);
+        combo->clear();
+
+        ssize_t cnt = libusb_get_device_list(NULL, &list);
+        for (ssize_t i = 0; i < cnt; i++) {
+            unsigned char buf[128] = {};
+            libusb_device_descriptor desc;
+            libusb_get_device_descriptor(list[i], &desc);
+            libusb_device_handle *h;
+            libusb_open(list[i], &h);
+            if (h) {
+                libusb_get_string_descriptor_ascii(h, desc.iProduct, buf, 128);
+                libusb_close(h);
+            }
+
+            std::ostringstream o;
+            o << std::hex << std::setfill('0');
+            o << std::setw(4) << desc.idVendor;
+            o << ':';
+            o << std::setw(4) << desc.idProduct;
+            o << ' ' << buf << std::endl;
+
+            combo->add(o.str().c_str());
+        }
+    }
+};
 
 int main(int argc, char **argv)
 {
@@ -15,32 +60,8 @@ int main(int argc, char **argv)
 
     auto w = new Fl_Window(600, 400, "USB Un-chan!");
 
-    auto devList = new Fl_Choice(100, 10, 100, 30, "device");
-
-    libusb_device **list;
-    ssize_t cnt = libusb_get_device_list(NULL, &list);
-    for (ssize_t i = 0; i < cnt; i++) {
-        unsigned char buf[128] = {};
-        libusb_device_descriptor desc;
-        libusb_get_device_descriptor(list[i], &desc);
-        libusb_device_handle *h;
-        libusb_open(list[i], &h);
-        if (h) {
-            libusb_get_string_descriptor_ascii(h, desc.iProduct, buf, 128);
-            libusb_close(h);
-        }
-
-        std::ostringstream o;
-        o << std::hex << std::setfill('0');
-        o << std::setw(4) << desc.idVendor;
-        o << ':';
-        o << std::setw(4) << desc.idProduct;
-        o << ' ' << buf << std::endl;
-
-        devList->add(o.str().c_str());
-    }
-    libusb_free_device_list(list, 1);
-
+    auto devList = DeviceList();
+    devList.Load();
 
     w->end();
     w->show(argc, argv);
