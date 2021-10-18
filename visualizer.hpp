7b47f1902b752256302ac267e3e24faca6121a42
Ben Masato <benmasato@kommu.ai>
2021-10-18 19:00:17 +0800

    Add DataRenderer to stringify data with render spec
    This allow implementation of data schema in the future via render spec
    abstraction (list of from, to and width).  Add representation type
    handling and data width handling for that purpose.

--- visualizer.hpp
+++ visualizer.hpp
@@ -4,12 +4,100 @@
 
 #include <iomanip>
 #include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+
+class DataRenderer {
+    enum Type {
+        CHAR,
+        HEX,
+        SIGNED,
+        UNSIGNED,
+    };
+
+    struct Spec {
+        int from, to, width;
+        Type t;
+    };
+
+    std::unordered_map<std::string, Type> const TypeMap;
+    std::vector<Spec> specs;
+
+public:
+    DataRenderer() : TypeMap{
+                        {"CHAR", Type::CHAR},
+                        {"HEX", Type::HEX},
+                        {"SIGNED", Type::SIGNED},
+                        {"UNSIGNED", Type::UNSIGNED},
+                     }
+    {
+        LoadDefaultSpec();
+    };
+
+    void LoadDefaultSpec()
+    {
+        specs.clear();
+        specs.push_back({
+                .from = 0,
+                .to = -1,
+                .width = 1,
+                .t = Type::HEX,
+        });
+    }
+
+    std::vector<std::pair<std::string, int>> Render(const char *buf, int len)
+    {
+        std::vector<std::pair<std::string, int>> r;
+
+        int i = 0, ctr = -1;
+        for (auto s: specs) {
+            ctr++;
+            // TODO: sanity check for from and to when loading
+            if (s.to == -1) {
+                if (ctr == (int) specs.size() - 1)
+                    s.to = len;
+                else
+                    s.to = specs[ctr + 1].from - 1;
+            }
+            for (;i < s.to;) {
+                std::ostringstream o;
+                char ibuf[8] = {};
+                // TODO: width check when loading
+                // TODO: signedness conversion
+                for (int j = 0; j < s.width; i++, j++)
+                    ibuf[j] = buf[i];
+
+                switch (s.t) {
+                case CHAR:
+                    o << (char) ibuf[0];
+                    break;
+                case HEX:
+                    o << std::hex << *((unsigned long long*) ibuf);
+                    break;
+                case SIGNED:
+                    o << *((long long*) ibuf);
+                    break;
+                case UNSIGNED:
+                    o << *((unsigned long long*) ibuf);
+                    break;
+                }
+                r.push_back({o.str(), s.width});
+            }
+        }
+
+        return r;
+    }
+};
+
 
 class VisualizerPanel {
     Fl_Pack *group;
     int rlen;
     const char *rbuf;
     int width;
+    DataRenderer renderer;
 
     void render()
     {
@@ -20,11 +108,10 @@ class VisualizerPanel {
         Fl_Pack *row = new Fl_Pack(0, 0, width, 30);
         row->box(FL_NO_BOX);
         row->type(Fl_Pack::HORIZONTAL);
-        for (int i = 0; i < rlen; i++) {
-            std::ostringstream o;
-            o << std::setw(2) << std::hex << std::setfill('0');
-            o << (int) rbuf[i];
-            if (x + 60 >= width) {
+
+        for (auto s: renderer.Render(rbuf, rlen)) {
+            int bwidth = 60 * s.second;
+            if (x + bwidth >= width) {
                 row->end();
                 row = new Fl_Pack(0, 0, width, 30);
                 row->box(FL_NO_BOX);
@@ -32,10 +119,10 @@ class VisualizerPanel {
                 x = 0;
                 rowCount++;
             }
-            auto b = new Fl_Button(0, 0, 60, 30, "");
-            b->copy_label(o.str().c_str());
+            auto b = new Fl_Button(0, 0, bwidth, 30, "");
+            b->copy_label(s.first.c_str());
             b->box(FL_FLAT_BOX);
-            x += 60;
+            x += bwidth;
         }
         row->end();
 
